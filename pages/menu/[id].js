70a83362832a6951f7c8410a91f5311daac7f568
Amin Moradi <aminmoradisaki13@gmail.com>
2025-07-05 20:09:57 +0330

    update

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -17,7 +17,7 @@ function Details({ data }) {
 export default Details;
 
 export async function getStaticPaths() {
-  const res = await fetch(`${process.env.BASE_URL}/data`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data`);
   const json = await res.json();
 
   const data = json.slice(0, 10);
@@ -34,7 +34,7 @@ export async function getStaticProps(context) {
     params: { id },
   } = context;
 
-  const res = await fetch(`${process.env.BASE_URL}/data/${id}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data/${id}`);
   const data = await res.json();
 
   if (!data.id) {
